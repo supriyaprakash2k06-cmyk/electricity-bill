fd6315ac586bda9907c2c86a759c4c4db92f33f4
Your Name <you@example.com>
2026-05-08 14:34:06 +0530

    final push

---
+++ jenkins.php
@@ -0,0 +1,36 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Clone Repository') {
+            steps {
+                git 'https://github.com/supriyaprakash2k06-cmyk/electricity-bill'
+            }
+        }
+
+        stage('Compile Java') {
+            steps {
+                bat 'javac ElectricityBill.java'
+            }
+        }
+
+        stage('Compile Test') {
+            steps {
+                bat 'javac -cp ".;junit-4.13.2.jar;hamcrest-core-1.3.jar" ElectricityBillTest.java'
+            }
+        }
+
+        stage('Run JUnit Test') {
+            steps {
+                bat 'java -cp ".;junit-4.13.2.jar;hamcrest-core-1.3.jar" org.junit.runner.JUnitCore ElectricityBillTest'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'Deployment Successful'
+            }
+        }
+    }
+}
